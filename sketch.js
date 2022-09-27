cb2277a15413f5a11b32e6e7074104645be9d090
tiaoffthewall <mattiabonanno@outlook.com>
2022-09-27 17:43:47 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background(0);  
+  rectMode(CENTER);
+  ellipseMode(CENTER);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  noStroke();
+  frameRate(10);
+ 
+  fill(random(255), random(255), random(255));
+  //draw a rectangle
+  let x = random(width);
+  let y = random(height);
+
+  let rWidth = random(width);
+  let rHeight = random(height);
+  drawingContext.filter = 'blur(300px)';
+  console.log('Frame rate: '+frameRate());
+  rect(x, y, rWidth, rHeight, 40, 80, 90); 
+  
 }
+  
+
